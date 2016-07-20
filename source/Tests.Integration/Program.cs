0462b5db1f3865b8765b2eb4ed45f1f5f83e28d8
neutmute <neutmute@users.noreply.github.com>
2016-07-20 14:07:01 +1000

    integration test for configure via code

---
+++ source/Tests.Integration/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Loggly.Config;
+using Multimeter;
+using Multimeter.Config;
+
+namespace Tests.Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureLoggly();
+
+            ConfigureMultimeter();
+
+            CreateMetrics();
+        }
+
+        private static void ConfigureMultimeter()
+        {
+            MultimeterConfig.Instance.Publishers.Add("Multimeter.Publisher.Loggly", "Multimeter.Publisher.Loggly.LogglyPublisher");
+        }
+
+        private static void CreateMetrics()
+        {
+            using (new SelfPublishingTimedMetric("Multimeter", "SelfPublishing"))
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void ConfigureLoggly()
+        {
+            var config = LogglyConfig.Instance;
+            config.CustomerToken = Environment.GetEnvironmentVariable("multimeter-loggly-customer-token");
+            config.ApplicationName = "multimeter-integration-test";
+
+            Console.WriteLine($"config.CustomerToken is {config.CustomerToken}");
+
+            config.Transport.EndpointHostname = "logs-01.loggly.com";
+            config.Transport.EndpointPort = 443;
+            config.Transport.LogTransport = LogTransport.Https;
+        }
+    }
+}
